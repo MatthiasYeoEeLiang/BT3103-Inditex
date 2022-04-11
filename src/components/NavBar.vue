653b8f2f74e0a4348af541b4f2ca32750e8bf763
LimNgeeFeng <limngeefengz@gmail.com>
2022-04-11 16:13:18 +0800

    views and router

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -30,6 +30,9 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Cart</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Account</a>
+        </li>
         <!-- <li class="nav-item">
           <a class="nav-link disabled"
             >Disabled</a
