97e94de80558e279cb2559ac0a8fc39543e62498
LimNgeeFeng <limngeefengz@gmail.com>
2022-04-11 13:55:12 +0800

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">INDITEX</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center " id="navbarNav">
+      <ul class="navbar-nav z">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Mix and Match</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Reviews</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Filter</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cart</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link disabled"
+            >Disabled</a
+          >
+        </li> -->
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+
+</style>
